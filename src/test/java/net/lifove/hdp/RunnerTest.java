c7916c40b1fff07b318a615b8ab71f551d5aed52
lifove <jc@lifove.net>
2016-05-03 13:22:23 -0400

    Update test case for options

--- src/test/java/net/lifove/hdp/RunnerTest.java
+++ src/test/java/net/lifove/hdp/RunnerTest.java
@@ -12,6 +12,24 @@ public class RunnerTest {
 		runner.runner(null);
 		
 		assertEquals(runner.sourcePath,null);
+		
+		runner = new Runner();
+		String[] args = {"-s", "test.arff"}; 
+		runner.runner(args);
+		assertEquals(runner.sourcePath,null); // because of missed options
+		
+		
+		String[] args2 = {"-s", "source.arff", "-t", "target.arff", "-l", "class", "-p", "buggy"}; 
+		runner.runner(args2);
+		
+		assertEquals(runner.sourcePath,args2[1]);
+		assertEquals(runner.targetPath,args2[3]);
+		assertEquals(runner.labelName,args2[5]);
+		assertEquals(runner.posLabelValue,args2[7]);
+		assertEquals(runner.cutoff,0.05,0);
+		assertEquals(runner.help,false);
+		assertEquals(runner.suppress,false);
+
 	}
 
 }
