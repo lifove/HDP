b929803df9790d9998b75af5aa8ffaa51f8729c2
lifove <jc@lifove.net>
2016-05-04 09:48:40 -0400

    Add Utils with loading an arff file

---
+++ src/main/java/net/lifove/hdp/util/Utils.java
@@ -0,0 +1,35 @@
+package net.lifove.hdp.util;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import weka.core.Instances;
+
+public class Utils {
+	/**
+	 * Load Instances from arff file. Last attribute will be set as class attribute
+	 * @param path arff file path
+	 * @return Instances
+	 */
+	public static Instances loadArff(String path,String classAttributeName){
+		Instances instances=null;
+		BufferedReader reader;
+		try {
+			reader = new BufferedReader(new FileReader(path));
+			instances = new Instances(reader);
+			reader.close();
+			instances.setClassIndex(instances.attribute(classAttributeName).index());
+		} catch (NullPointerException e) {
+			System.err.println("Class label name, " + classAttributeName + ", does not exist! Please, check if the label name is correct.");
+			instances = null;
+		} catch (FileNotFoundException e) {
+			System.err.println("Data file does not exist. Please, check the path again!");
+		} catch (IOException e) {
+			System.err.println("I/O error! Please, try again!");
+		}
+
+		return instances;
+	}
+}
